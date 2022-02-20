9c010d64f1037758bf96421d1636963acad2d6d6
robertramosastudillo <robertramosastudillo@gmail.com>
2022-02-20 10:25:54 -0500

    Anchor navigation in mobile is adjusted

--- src/components/Navbar/Navbar.style.ts
+++ src/components/Navbar/Navbar.style.ts
@@ -32,3 +32,125 @@ export const Logo = styled.img`
     height: 22px;
   }
 `;
+
+// BURGER MENU
+export const StyledBurger: any = styled.div`
+  width: 2rem;
+  height: 2rem;
+  position: fixed;
+  top: 15px;
+  right: 20px;
+  z-index: 20;
+  display: none;
+  cursor: pointer;
+
+  @media (max-width: 768px) {
+    display: flex;
+    justify-content: space-around;
+    flex-flow: column nowrap;
+  }
+
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    background-color: ${({ open }: any) => (open ? "#fff" : "#6838F2")};
+    border-radius: 10px;
+    transform-origin: 1px;
+    transition: all 0.3s linear;
+
+    &:nth-child(1) {
+      transform: ${({ open }: any) => (open ? "rotate(45deg)" : "rotate(0)")};
+      top: 0;
+    }
+
+    &:nth-child(2) {
+      transform: ${({ open }: any) =>
+        open ? "translateX(100%)" : "translateX(0)"};
+      opacity: ${({ open }: any) => (open ? 0 : 1)};
+    }
+
+    &:nth-child(3) {
+      transform: ${({ open }: any) => (open ? "rotate(-45deg)" : "rotate(0)")};
+      bottom: 0;
+    }
+  }
+`;
+// BURGER MENU
+
+// RIGHT NAVBAR
+
+export const Ul: any = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
+  color: #6838f2;
+
+  li {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    padding: 18px 10px;
+    cursor: pointer;
+
+    &:hover {
+      text-decoration: underline;
+    }
+
+    &:last-of-type {
+      padding-right: 0;
+    }
+  }
+
+  div {
+    display: none;
+  }
+
+  @media (max-width: 768px) {
+    div {
+      display: block;
+      padding: 40px 20px 10px 20px;
+      font-size: 20px;
+      line-height: 25px;
+
+      h4 {
+        color: #ffffff;
+        font-weight: 500;
+      }
+
+      p {
+        color: #caaffd;
+        font-weight: 400;
+      }
+    }
+
+    flex-flow: column nowrap;
+    background-color: #6838f2;
+    position: fixed;
+    transform: ${({ open }: any) =>
+      open ? "translateX(0)" : "translateX(100%)"};
+    top: 0;
+    right: 0;
+    height: 100vh;
+    width: 100%;
+    padding-top: 3.5rem;
+    transition: transform 0.3s ease-in-out;
+
+    &:after {
+      position: absolute;
+      /* content: url("/logo-background-icon-2.svg"); */
+    }
+
+    li {
+      color: #fff;
+      padding: 8px 20px;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    li {
+      padding: 18px 24px;
+    }
+  }
+`;
+
+// RIGHT NAVBAR
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,77 @@
-import { BurgerMenu } from "../BurgerMenu/BurgerMenu";
-import { Logo, NavbarStyle } from "./Navbar.style";
+import { Logo, NavbarStyle, StyledBurger, Ul } from "./Navbar.style";
 import { Link } from "react-scroll";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <NavbarStyle>
-      <Link to="banner-main" spy={true} smooth={true} duration={500}>
-      <Logo src="/logo.svg" alt="Robert Ramos" />
+      <Link to="banner-main" spy={true} smooth={true} duration={800}>
+        <Logo src="/logo.svg" alt="Robert Ramos" />
       </Link>
-      <BurgerMenu />
+      <StyledBurger open={open} onClick={() => setOpen(!open)}>
+        <div />
+        <div />
+        <div />
+      </StyledBurger>
+      <Ul open={open}>
+        <div>
+          <Link
+            to="banner-main"
+            spy={true}
+            smooth={true}
+            duration={800}
+            onClick={() => setOpen(!open)}
+          >
+            <h4>Robert Ramos</h4>
+            <p>Frontend Developer</p>
+          </Link>
+        </div>
+        <li>
+          <Link
+            to="about-me"
+            spy={true}
+            smooth={true}
+            duration={800}
+            onClick={() => setOpen(!open)}
+          >
+            01. Sobre mí
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="experience"
+            spy={true}
+            smooth={true}
+            duration={800}
+            onClick={() => setOpen(!open)}
+          >
+            02. Experiencia
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="my-job"
+            spy={true}
+            smooth={true}
+            duration={800}
+            onClick={() => setOpen(!open)}
+          >
+            03. Mi trabajo
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="contact"
+            spy={true}
+            smooth={true}
+            duration={800}
+            onClick={() => setOpen(!open)}
+          >
+            04. Contacto
+          </Link>
+        </li>
+      </Ul>
     </NavbarStyle>
   );
 };
