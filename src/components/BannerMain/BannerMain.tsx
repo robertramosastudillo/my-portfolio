46e49cf4aec17406e07d512c06c2660ef09439bd
robertramosastudillo <robertramosastudillo@gmail.com>
2022-02-20 17:42:58 -0500

    refactor text banner

--- src/components/BannerMain/BannerMain.tsx
+++ src/components/BannerMain/BannerMain.tsx
@@ -23,7 +23,7 @@ export const BannerMain = () => {
         </TitleBanner>
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industries standard dummy text ever
           since the 1500s.
         </p>
         <ContainerButtons>
