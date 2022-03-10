7b1a27b16a1ea5415b44a6c6bf0975ab5bd10ac7
robertramosastudillo <robertramosastudillo@gmail.com>
2022-03-10 10:04:09 -0500

    refactor css to ssr

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,24 +1,50 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
+import MyApp from "./_app";
 
-export default function Document() {
-  return (
-    <Html lang="es">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&family=Roboto:wght@400;700;900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static getInitialProps = async (ctx: any) => {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (MyApp: any) => (props: any) =>
+            sheet.collectStyles(<MyApp {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  };
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+        </Head>
+        <body>
+          <Main />
+          <div id="portal" />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
