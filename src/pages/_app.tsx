e0cb84e06408a2e4e322f1f9a37233ac26a91008
robertramosastudillo <robertramosastudillo@gmail.com>
2022-03-12 11:55:32 -0500

    refactor GTM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,36 +1,14 @@
 import type { AppProps } from "next/app";
-import { Router, useRouter } from "next/router";
 import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 import Layout from "../components/Layout/Layout";
 import { Seo } from "../components/Seo/Seo";
 import GlobalStyle from "../styles/GlobalStyle";
 
-declare global {
-  interface Window {
-    dataLayer: any;
-  }
-}
-
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const pageView = (url: any, title: any) => {
-    window &&
-      window.dataLayer &&
-      window.dataLayer.push({
-        event: "virtualPageview",
-        virtualPageURL: url,
-        virtualPageTitle: title,
-      });
-  };
+
   useEffect(() => {
-    pageView(router.pathname, document.title);
-    const handleRouteChange = (url: any) => {
-      pageView(url, document.title);
-    };
-    Router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      Router.events.off("routeChangeComplete", handleRouteChange);
-    };
+    TagManager.initialize({ gtmId: "GTM-MMJG7J9" });
   }, []);
 
   return (
