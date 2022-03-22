f7fab9725422621286b5b7b58d32747c6f48326b
robertramosastudillo <robertramosastudillo@gmail.com>
2022-03-22 16:15:49 -0500

    refactor SEO meta tags

--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -10,6 +10,21 @@ export const Seo = () => {
         name="description"
         content="Desarrollador Full Stack en tecnologias javascript. Especializado en el stack MERN (MongoDB, ExpressJS, ReactJS Y NodeJS)."
       />
+      <meta
+        property="og:title"
+        content="Robert Ramos | Full Stack Developer"
+      />
+      <meta
+        property="og:description"
+        content="Desarrollador Full Stack en tecnologias javascript. Especializado en el stack MERN (MongoDB, ExpressJS, ReactJS Y NodeJS)."
+      />
+      <meta
+        property="og:image"
+        content="banner-main/profile.png"
+      />
+      <meta property="og:url" content="https://robertramosastudillo.com" />
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="es_CO" />
     </Head>
   );
 };
